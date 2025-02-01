b9bc96121ae0966ccc3156760892ffb1f1f2a7cb
PranavBhandawat <pranavbhandawat@gmail.com>
2025-02-01 16:45:38 +0530

    Changed the logo to the new one.

--- src/components/Auths/DefaultAuthCard.vue
+++ src/components/Auths/DefaultAuthCard.vue
@@ -11,8 +11,8 @@ const props = defineProps(['subtitle', 'title', 'customClasses'])
       <div class="hidden w-full xl:block xl:w-1/2 bg-indigo-50">
         <div class="py-17.5 px-26 text-center">
           <router-link class="mb-5.5 inline-block" to="/">
-            <img class="hidden dark:block zengarden-logo" src="../../assets/images/logo/logo-light.png" alt="Logo" />
-            <img class="dark:hidden zengarden-logo" src="../../assets/images/logo/logo-light.png" alt="Logo" />
+            <img class="hidden dark:block zengarden-logo" src="../../assets/images/logo/Zengarden Logo-28.png" alt="Logo" />
+            <img class="dark:hidden zengarden-logo" src="../../assets/images/logo/Zengarden Logo-28.png" alt="Logo" />
           </router-link>
 
           <p class="font-medium 2xl:px-20">Login to Your Emotional Headquarters</p>
