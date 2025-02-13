3afd8e7a4bd68787e37da78a23bfbfdba5413ede
Aditya Saxena <aaddii091@gmail.com>
2025-02-13 01:28:52 +0100

    Asset Management

--- src/components/Header/HeaderArea.vue
+++ src/components/Header/HeaderArea.vue
@@ -15,7 +15,7 @@ const sidebarStore = useSidebarStore()
   <header
     class="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none"
   >
-    <!-- SIDEBAR HEADER -->
+    <!-- SIDEBAR HEADER --->
     <div class="flex flex-grow items-center justify-between shadow-2 md:px-6 2xl:px-11">
       <div class="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <router-link to="/">
