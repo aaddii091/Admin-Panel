0bc708a69a77406242fcea83c30e9acfb3737e04
Aditya Saxena <aaddii091@gmail.com>
2025-03-07 19:17:47 +0100

    resetPassword Phases

--- src/components/Authentication/ResetPasswordView.vue
+++ src/components/Authentication/ResetPasswordView.vue
@@ -1,45 +1,44 @@
 <template>
-  <!-- Breadcrumb Start -->
-
-  <!-- Breadcrumb End -->
-
   <ResetPasswordAuthCard
     title="Reset Password"
     class="mx-4 sm:mx-8 md:mx-12 lg:mx-18 mt-6 sm:mt-8 md:mt-10 lg:mt-12"
   >
-    <form @submit="handleSubmit">
-      <!-- Show error message if exists -->
-      <div v-if="error" class="mb-4 p-4 bg-red-100 text-red-600 rounded-lg">
-        {{ error }}
-      </div>
+    <div class="phaseOne" v-if="!authToken">
+      <form @submit="handleSubmit">
+        <!-- Show error message if exists -->
+        <div v-if="error" class="mb-4 p-4 bg-red-100 text-red-600 rounded-lg">
+          {{ error }}
+        </div>
 
-      <p class="mb-10 block font-medium text-black dark:text-white"> Please submit your email address. We will send you a link to reset your Password.</p>
+        <p class="mb-10 block font-medium text-black dark:text-white">
+          Please submit your email address. We will send you a link to reset your Password.
+        </p>
 
-      <InputGroup
-        label="Your Email Address"
-        type="email"
-        placeholder="john.dowry@example.com"
-        v-model="email"
-        required
-      >
-        <svg
-          class="fill-current"
-          width="22"
-          height="22"
-          viewBox="0 0 22 22"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+        <InputGroup
+          label="Your Email Address"
+          type="email"
+          placeholder="john.dowry@example.com"
+          v-model="email"
+          required
         >
-          <g opacity="1">
-            <path
-              d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-              fill="#737373"
-            />
-          </g>
-        </svg>
-      </InputGroup>
+          <svg
+            class="fill-current"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="1">
+              <path
+                d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
+                fill="#737373"
+              />
+            </g>
+          </svg>
+        </InputGroup>
 
-      <!--<InputGroup
+        <!--<InputGroup
         label="Password"
         type="password"
         placeholder="6+ Characters, 1 Capital letter"
@@ -81,22 +80,75 @@
         <input type="checkbox" class="remember-me mr-2" />Remember Me
       </div> -->
 
-      <div class="mb-5 mt-10">
-        <button
-          type="submit"
-          :disabled="loading"
-          class="w-full cursor-pointer rounded-full border border-primary bg-primary py-3 pl-4 p-4 font-medium text-white transition hover:bg-opacity-90 disabled:opacity-50"
-        >
-          {{ loading ? 'Signing in...' : 'Submit' }}
-        </button>
-      </div>
+        <div class="mb-5 mt-10">
+          <button
+            type="submit"
+            :disabled="loading"
+            class="w-full cursor-pointer rounded-full border border-primary bg-primary py-3 pl-4 p-4 font-medium text-white transition hover:bg-opacity-90 disabled:opacity-50"
+          >
+            {{ loading ? 'Signing in...' : 'Submit' }}
+          </button>
+        </div>
 
-      <div class="mt-6 text-center">
-        <p class="font-medium">
-          <router-link to="signup" class="text-primary">Resend Link</router-link>
+        <div class="mt-6 text-center">
+          <p class="font-medium">
+            <router-link to="signup" class="text-primary">Resend Link</router-link>
+          </p>
+        </div>
+      </form>
+    </div>
+    <div class="phaseTwo" v-else>
+      <form @submit="handleSubmit">
+        <!-- Show error message if exists -->
+        <div v-if="error" class="mb-4 p-4 bg-red-100 text-red-600 rounded-lg">
+          {{ error }}
+        </div>
+
+        <p class="mb-10 block font-medium text-black dark:text-white">
+          Please enter your new password
         </p>
-      </div>
-    </form>
+
+        <InputGroup
+          label="Your Email Address"
+          type="email"
+          placeholder="john.dowry@example.com"
+          v-model="email"
+          required
+        >
+          <svg
+            class="fill-current"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="1">
+              <path
+                d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
+                fill="#737373"
+              />
+            </g>
+          </svg>
+        </InputGroup>
+
+        <div class="mb-5 mt-10">
+          <button
+            type="submit"
+            :disabled="loading"
+            class="w-full cursor-pointer rounded-full border border-primary bg-primary py-3 pl-4 p-4 font-medium text-white transition hover:bg-opacity-90 disabled:opacity-50"
+          >
+            {{ loading ? 'Signing in...' : 'Submit' }}
+          </button>
+        </div>
+
+        <div class="mt-6 text-center">
+          <p class="font-medium">
+            <router-link to="signup" class="text-primary">Resend Link</router-link>
+          </p>
+        </div>
+      </form>
+    </div>
   </ResetPasswordAuthCard>
 </template>
 <script setup lang="ts">
@@ -115,6 +167,7 @@ const email = ref('')
 const password = ref('')
 const error = ref('')
 const loading = ref(false)
+const authToken = ref(true)
 
 const handleSubmit = async (e: Event) => {
   e.preventDefault()
@@ -161,5 +214,4 @@ const handleSubmit = async (e: Event) => {
   display: flex;
   justify-content: center;
 }
-
 </style>
