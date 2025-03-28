47ea8d0dc3fbbd7c8c2a1051fbc7850c5d2d97a1
PranavBhandawat <pranavbhandawat@gmail.com>
2025-03-28 12:12:01 +0530

    Dashboard page- Added a text above Table component

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -16,9 +16,12 @@ const username = 'x';
       <h2 class="font-bold text-[28px] leading-[36px]">Welcome, {{ username }}!</h2>
       <h2 class="font-medium text-[18px] leading-[28px]">Discover qualified psychologists tailored to your needs.</h2>
     </div>
-    <div class="flex justify-around gap=32px pb-8 bg-white"> 
+    <div class="flex justify-around gap=32px pb-12 bg-white"> 
       <TestCount label="Assigned Tests" background="bg-[#EBEDFF]" :count="assignedTests"/>
       <TestCount label="Completed Tests" background="bg-[#FFF5EB]" :count="completedTests"/>
+    </div>
+    <div>
+      <h2 class="bg-white pl-7 text-black font-bold text-[20px]">Recent Test Reports</h2>
     </div>
     <QuizList />
   </DefaultLayout>
