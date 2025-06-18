a00ed4f53b4af20806968ccc73ef3909b25e0c3a
PranavBhandawat <pranavbhandawat@gmail.com>
2025-06-18 18:00:40 +0530

    Created a Dashboard on the Admin Page

---
+++ src/views/AdminPanel/Dashboard/DashboardView.vue
@@ -0,0 +1,14 @@
+<reference path="path/types.d.ts" />
+<script setup lang="ts">
+import AdminLayout from '@/layouts/AdminLayout.vue';
+
+</script>
+
+<template>
+  <AdminLayout>
+  </AdminLayout>
+</template>
+
+<style>
+
+</style>
