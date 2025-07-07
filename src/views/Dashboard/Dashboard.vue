a49b40d3971b65a99da815a6a302b020d59e07dd
PranavBhandawat <pranavbhandawat@gmail.com>
2025-07-07 17:52:32 +0530

    TestCard made responsive

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -17,19 +17,42 @@ defineOptions({
 <template>
   <DefaultLayout>
     <div class="mb-4">
+      <div class="bg-white text-black py-7 pt-6 pb-6 shadow-default dark:bg-boxdark flex flex-col">
+        <div class="pl-7 pt-6 pb-6">
+          <h2 class="font-bold text-[28px] leading-[36px] dark:text-white">Welcome, {{ username }}!</h2>
+          <h2 class="font-medium text-[18px] leading-[28px] dark:text-[#AEAEAE]">Discover qualified psychologists tailored to your needs.</h2>
+        </div>
+        <!-- <div class="flex justify-around w-1/2 gap=32px pb-12">
+          <TestCount label="Assigned Tests" background="bg-[#EBEDFF]" :count="assignedTests"/>
+          <TestCount label="Completed Tests" background="bg-[#FFF5EB]" :count="completedTests"/>
+        </div> -->
+        <div class="flex flex-col sm:flex-row justify-center gap-y-4 pb-12 bg-white dark:bg-boxdark">
+          <div class="flex justify-center w-full">
+            <TestCount label="Assigned Tests" background="bg-[#EBEDFF]" :count="assignedTests" />
+          </div>
+          <div class="flex justify-center w-full">
+            <TestCount label="Completed Tests" background="bg-[#FFF5EB]" :count="completedTests" />
+          </div>
+        </div>
+
+        <div class="">
+          <h2 class="pl-7 text-black font-bold text-[20px] dark:text-white">Recent Test Reports</h2>
+        </div>
+          <QuizList />
+      </div>
     </div>
-    <div class="bg-white text-black pl-7 pt-6 pb-6 dark:bg-boxdark">
+    <!-- <div class="bg-white text-black pl-7 pt-6 pb-6 dark:bg-boxdark">
       <h2 class="font-bold text-[28px] leading-[36px] dark:text-white">Welcome, {{ username }}!</h2>
       <h2 class="font-medium text-[18px] leading-[28px] dark:text-[#AEAEAE]">Discover qualified psychologists tailored to your needs.</h2>
     </div>
-    <div class="flex justify-around gap=32px pb-12 bg-white dark:bg-boxdark"> 
-      <TestCount label="Assigned Tests" background="bg-[#EBEDFF]" :count="assignedTests"/>
-      <TestCount label="Completed Tests" background="bg-[#FFF5EB]" :count="completedTests"/>
+    <div class="flex justify-around w-1/2 gap=32px pb-12 bg-white dark:bg-boxdark">
+        <TestCount label="Assigned Tests" background="bg-[#EBEDFF]" :count="assignedTests"/>
+        <TestCount label="Completed Tests" background="bg-[#FFF5EB]" :count="completedTests"/>
     </div>
     <div class="bg-white dark:bg-boxdark">
       <h2 class="pl-7 text-black font-bold text-[20px] dark:text-white">Recent Test Reports</h2>
     </div>
-    <QuizList />
+    <QuizList /> -->
   </DefaultLayout>
 </template>
 
